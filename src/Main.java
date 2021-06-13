0407fdd2602c8f5694eb88301c583e4a97f56ee9
K2K <k2k.adam.t@gmail.com>
2021-06-13 15:30:05 +0200

    testing how Mathematical operators are working

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,30 @@ public class Main {
 //        System.out.println(Character.isDigit(Znaczek));
 //        System.out.println(Alpha);
 
-        String text = " Duke Nukem Forever ";
-        String text2 = new String("Olga2");
-        String text3 = "Olga3";
-        System.out.println(text.length());
-        System.out.println(text.indexOf('g'));
-        System.out.println(text.substring(3,7));
-        System.out.println(text.trim());
-        text.toCharArray();
+//        String text = " Duke Nukem Forever ";
+//        String text2 = new String("Olga2");
+//        String text3 = "Olga3";
+//        System.out.println(text.length());
+//        System.out.println(text.indexOf('g'));
+//        System.out.println(text.substring(3,7));
+//        System.out.println(text.trim());
+//        text.toCharArray();
+
+        int a;
+        int b;
+        a = 10;
+        b = 50;
+        int suma = a+b;
+        int roznica = a-b;
+        float mnozenie = a*b;
+        double dzielenie = a/b;
+        int modulo = a%b;
+        System.out.println("Wartość a to "+a);
+        System.out.println("Wartość b to "+b);
+        System.out.println("Suma a i b to: "+ suma);
+        System.out.println("Roznica a i b to: "+ roznica);
+        System.out.println("Mnozenie a i b daje: "+ mnozenie);
+        System.out.println("Dzielenie a i b daje: "+ dzielenie);
+        System.out.println("Reszta z dzielenia a i b daje: " +modulo);
     }
 }
