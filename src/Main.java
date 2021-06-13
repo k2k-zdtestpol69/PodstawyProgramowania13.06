4cac554f1924314035617983543e454a74fad303
K2K <k2k.adam.t@gmail.com>
2021-06-13 14:51:43 +0200

    testing how char and Character.isDigit is working

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Cześć Adam");
+//        System.out.println("Hello World!");
+//        System.out.println("Cześć Adam");
+//
+//
+//        Car Opel = new Car();
+//        Car Kia = new Car("Kia","ceed","srebrny","2018","petrol");
+//
+//        Kia.StartEngine();
+//
+//        Kia.StopEngine(100);
+//
+//        System.out.println("Cześć Adam\tblablabla");
+//        System.out.println("Cześć Adam\nnowa linia hehehe");
+//        System.out.println("Cześć Adam \"interpreter omija\"");
+//        // komentarzyk blablabla niewidoczny dla IntelliJ
+//
+//        Opel.StartEngine();
 
+        String text = "Podstawy programowania JAVA";
+        char Znaczek = 'p';
+        char Alpha = '\u03b1';
 
-        Car Opel = new Car();
-        Car Kia = new Car("Kia","ceed","srebrny","2018","petrol");
+        System.out.println(text);
 
-        Kia.StartEngine();
-
-        Kia.StopEngine(100);
-
-        System.out.println("Cześć Adam\tblablabla");
-        System.out.println("Cześć Adam\nnowa linia hehehe");
-        System.out.println("Cześć Adam \"interpreter omija\"");
-
-        // komentarzyk blablabla niewidoczny dla IntelliJ
-
-        Opel.StartEngine();
+        System.out.println(Character.isDigit(Znaczek));
+        System.out.println(Alpha);
 
     }
 }
