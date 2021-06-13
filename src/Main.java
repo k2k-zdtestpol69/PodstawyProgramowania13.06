fd048eaeccccddae599c5047ae4c2f3b884c9b9c
K2K <k2k.adam.t@gmail.com>
2021-06-13 15:13:07 +0200

    testing how Strings are working and JAVA functions on these

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
 //        System.out.println("Hello World!");
 //        System.out.println("Cześć Adam");
 //
-//
+//          ** Constructing Cars
 //        Car Opel = new Car();
 //        Car Kia = new Car("Kia","ceed","srebrny","2018","petrol");
 //
 //        Kia.StartEngine();
 //
 //        Kia.StopEngine(100);
+
+//          ** How to make interpreters ommit
 //
 //        System.out.println("Cześć Adam\tblablabla");
 //        System.out.println("Cześć Adam\nnowa linia hehehe");
@@ -20,14 +22,24 @@ public class Main {
 //
 //        Opel.StartEngine();
 
-        String text = "Podstawy programowania JAVA";
-        char Znaczek = 'p';
-        char Alpha = '\u03b1';
 
-        System.out.println(text);
+        /*Chars And Strings*/
+//        String text = "Podstawy programowania JAVA";
+//        char Znaczek = 'p';
+//        char Alpha = '\u03b1';
+//
+//        System.out.println(text);
+//
+//        System.out.println(Character.isDigit(Znaczek));
+//        System.out.println(Alpha);
 
-        System.out.println(Character.isDigit(Znaczek));
-        System.out.println(Alpha);
-
+        String text = " Duke Nukem Forever ";
+        String text2 = new String("Olga2");
+        String text3 = "Olga3";
+        System.out.println(text.length());
+        System.out.println(text.indexOf('g'));
+        System.out.println(text.substring(3,7));
+        System.out.println(text.trim());
+        text.toCharArray();
     }
 }
